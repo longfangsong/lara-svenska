2318026ab3e9463e8d78de8f2d3b6f910ece33d3
longfangsong <longfangsong@icloud.com>
2024-09-02 23:24:27 +0200

    disable ReviewButton after click

--- src/app/_components/ReviewButton.tsx
+++ src/app/_components/ReviewButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { WordReview } from "@/types";
 import { Button } from "flowbite-react";
+import { useState } from "react";
 import { IoCheckmarkDoneOutline } from "react-icons/io5";
 async function updateWordReview(review: WordReview) {
   const payload = {
@@ -17,18 +18,21 @@ async function updateWordReview(review: WordReview) {
 }
 export function ReviewButton({
   review,
-  onClick
+  onClick,
 }: {
   review: WordReview;
   onClick?: () => void;
 }) {
+  const [clicked, setClicked] = useState(false);
   return (
     <Button
       className="ml-3 p-0"
       onClick={async () => {
         updateWordReview(review);
         onClick && onClick();
+        setClicked(true);
       }}
+      disabled={clicked}
     >
       <IoCheckmarkDoneOutline className="h-4 w-4" />
     </Button>
